36b40ad846e3699e398dcfc75d325a3d64319d7f
Diego Anfuso <diego.anfuso@bactrack.com>
2025-05-06 17:16:52 -0300

    Get and set units of measure.

--- app/src/main/java/com/bactrack/bactrack_mobile/android_c6_config/MainActivity.java
+++ app/src/main/java/com/bactrack/bactrack_mobile/android_c6_config/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -35,11 +36,12 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
     private TextView connectionTextView;
     private Button getUnitsButton;
     private Button setUnits1Button;
-    private Button setUnits2Button;
     private Button disconnectButton;
     private Button connectButton;
 
     private BACtrackAPI mAPI;
+
+    private final BACtrackUnit selected = BACtrackUnit.BACtrackUnit_permille_2100_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +54,6 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
         this.getUnitsButton = findViewById(R.id.get_units_of_measure_button_id);
         this.setUnits1Button = findViewById(R.id.set_units_of_measure_button_id);
         this.disconnectButton = findViewById(R.id.disconnect_button_id);
-        this.setUnits2Button = findViewById(R.id.set_units_of_measure_2_button_id);
 
         connectButton.setOnClickListener(v -> {
             connectNearestClicked();
@@ -63,11 +64,7 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
         });
 
         setUnits1Button.setOnClickListener(v -> {
-            setUnitsClicked(BACtrackUnit.BACtrackUnit_permille);
-        });
-
-        setUnits2Button.setOnClickListener(v -> {
-            setUnitsClicked(BACtrackUnit.BACtrackUnit_permille2);
+            setUnitsClicked();
         });
 
         disconnectButton.setOnClickListener(v -> {
@@ -203,6 +200,18 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
 
     @Override
     public void BACtrackUnits(final BACtrackUnit baCtrackUnit) {
+        String actualString = unitsToString(baCtrackUnit);
+        BACtrackUnit expectedUnit = selected;
+        String expectedString = unitsToString(expectedUnit);
+        if (baCtrackUnit == expectedUnit) {
+            setStatus("Units set to expected value.\n" + expectedString, true);
+        } else {
+            setStatus("Units NOT set to expected value.\nActual: " + actualString, false);
+        }
+    }
+
+    @NonNull
+    private static String unitsToString(BACtrackUnit baCtrackUnit) {
         String units;
         switch (baCtrackUnit) {
             case BACtrackUnit_bac:
@@ -211,11 +220,14 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
             case BACtrackUnit_mgL:
                 units = "mg/L";
                 break;
-            case BACtrackUnit_permille:
-                units = "Permille 2100:1 (4)";
+            case BACtrackUnit_permille_2300_1:
+                units = "Permille % - 2300:1 (3)";
                 break;
-            case BACtrackUnit_permille2:
-                units = "Permille 2000:1 (5)";
+            case BACtrackUnit_permille_2100_1:
+                units = "Permille % - 2100:1 (4)";
+                break;
+            case BACtrackUnit_permille_2000_1:
+                units = "Permille % - 2000:1 (5)";
                 break;
             case BACtrackUnit_permilleByMass:
                 units = "Permille by mass";
@@ -226,7 +238,7 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
             default:
                 units = "UNKNOWN";
         }
-        setStatus("Units of measure:\n" + units);
+        return units;
     }
 
     private void requestPermissions() {
@@ -261,11 +273,11 @@ public class MainActivity extends Activity implements BACtrackAPICallbacks {
         }
     }
 
-    private void setUnitsClicked(final BACtrackUnit baCtrackUnit) {
+    private void setUnitsClicked() {
         if (mAPI != null) {
-            boolean result = mAPI.writeUnitsToDevice(baCtrackUnit);
+            boolean result = mAPI.writeUnitsToDevice(selected);
             if (result) {
-                getUnitsClicked();
+                setStatus("Units set to\n" + unitsToString(selected));
             } else {
                 setStatus("Failed to set units", false);
             }
